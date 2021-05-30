8b5457d2642ce9b3db5c045280fadee20a1d7480
kurniacf <kcf170202@gmail.com>
2021-05-30 16:11:31 +0700

    added

--- delete.php
+++ delete.php
@@ -1,17 +1,10 @@
 <?php
 include_once('koneksi.php');
 
-<<<<<<< HEAD
-    if(!empty($_POST['id'])){
-        $id = $_POST['id'];
-        $query = "DELETE FROM Content WHERE id = '$id'";
-        $delete = mysqli_query($connect, $query);
-=======
 if (!empty($_POST['id'])) {
     $id = $_POST['id'];
     $query = "DELETE FROM Content WHERE id = '$id'";
     $delete = mysqli_query($connect, $query);
->>>>>>> 189c1558bfc0f15ecf3a2f5ff45d31215047a4ad
 
     if ($delete) {
         set_response(true, "Success delete");
--- getData.php
+++ getData.php
@@ -2,29 +2,12 @@
 
 include_once('koneksi.php');
 
-<<<<<<< HEAD
 if (!empty($_GET['id'])) {
     $id = $_GET['id'];
     $query = "SELECT * FROM Content WHERE id = '$id'";
 } else {
     $query = "SELECT * FROM Content";
 }
-=======
-    if(!empty($_GET['id'])){
-        $id = $_GET['id'];
-        $query = "SELECT * FROM Content WHERE id = '$id'";
-    }else{
-        $query = "SELECT * FROM Content";
-    }
-
-    $get = mysqli_query($connect, $query);
-    $data = array();
-
-    if(mysqli_num_rows($get) > 0){
-        while($row = mysqli_fetch_assoc($get)){
-            $data[] = $row;
-        }
->>>>>>> 189c1558bfc0f15ecf3a2f5ff45d31215047a4ad
 
 $get = pg_query($connect, $query);
 $data = array();
