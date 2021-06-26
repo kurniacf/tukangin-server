08699bb3532765045ea288b586d541303eeeb540
kurniacf <kcf170202@gmail.com>
2021-06-26 12:13:07 +0700

    fix

--- koneksi.php
+++ koneksi.php
@@ -13,6 +13,7 @@ $password = "d61f62e3127022400fb6816ed6e5f400e0c4c099a8e27ab728f3f7bef201f6cf";
 $dbname = "d1msbotm7r9qbo";
 $port = "5432";
 
+//
 $connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password}";
 $connect = pg_connect($connection_string);
 
