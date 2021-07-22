eeeb0d503e4105a4a2b5055844dd105514bfda57
kurniacf <70510279+kurniacf@users.noreply.github.com>
2021-07-22 12:53:37 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,17 +1,9 @@
 <?php
-//local
-// $host = "localhost";
-// $port = "5432";
+// $host = "";
+// $port = ";
 // $user = "";
 // $password = "";
 // $dbname = "";
-
-//heroku
-$host = "ec2-52-4-111-46.compute-1.amazonaws.com";
-$user = "ejexbwyopvqhbx";
-$password = "d61f62e3127022400fb6816ed6e5f400e0c4c099a8e27ab728f3f7bef201f6cf";
-$dbname = "d1msbotm7r9qbo";
-$port = "5432";
 
 //
 $connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password}";
